9f2457a56da26326db5fd191165a36d77f463807
tangfeng <wb-tf492002@antfin.com>
2019-03-28 11:11:01 +0800

    feat: add fastClick

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import FastClick from 'fastclick';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+FastClick.attach(document.body);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
